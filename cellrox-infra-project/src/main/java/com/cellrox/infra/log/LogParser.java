998675605fd208c946b9a65bc53e654e2e0f3c3b
nirarmon <nir.armon33@gmail.com>
2013-12-22 13:03:50 +0200

    log

---
+++ cellrox-infra-project/src/main/java/com/cellrox/infra/log/LogParser.java
@@ -0,0 +1,56 @@
+package com.cellrox.infra.log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.cellrox.infra.object.LogParserExpression;
+
+import jsystem.extensions.analyzers.text.CountText;
+import jsystem.framework.report.Reporter.ReportAttribute;
+import jsystem.framework.scenario.SystemObjectOperation;
+import jsystem.utils.FileUtils;
+
+public class LogParser extends SystemObjectOperation {
+
+	LogParserExpression[] expressions;
+
+	public LogParser(LogParserExpression[] expressions) {
+		super();
+		this.expressions = expressions;
+	}
+
+	File[] logs;
+
+	public void validateLogs() throws IOException {
+		for (File logFile : logs) {
+			String log = FileUtils.read(logFile);
+			for (LogParserExpression expression : expressions) {
+				CountText countText = new CountText(expression.getExpression(), 0, 0, true);
+				countText.setTestAgainst(log);
+				countText.analyze();
+				int result = countText.getActualCount();
+				if (result > 0) {
+					log = log.replace(expression.getExpression(), "<b><font size=\"4\" "+expression.getHtmlColor()+">" + expression.getExpression()
+							+ "</font></b>");
+					report.report(
+							"found error " + expression.getNiceName() + " in "
+									+ logFile.getName(), report.FAIL);
+				}
+			}
+			// print log
+			log = log.replace("\n", "<br>");
+			report.report("Click Here to See Results " + logFile.getName(),
+					log, ReportAttribute.HTML);
+		}
+	}
+
+	public File[] getLogs() {
+		return logs;
+	}
+
+	public void setLogs(File... logs) {
+		this.logs = logs;
+	}
+
+}
