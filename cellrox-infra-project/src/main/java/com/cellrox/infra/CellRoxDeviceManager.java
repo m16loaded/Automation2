4247410e2df8f4c198523f980bae789182e1c235
orgar <orgarfunkel@gmail.com>
2014-01-29 17:32:20 +0200

    this CellRoxDeviceManager is the manget of the multi device and it ceates them

---
+++ cellrox-infra-project/src/main/java/com/cellrox/infra/CellRoxDeviceManager.java
@@ -0,0 +1,114 @@
+package com.cellrox.infra;
+
+import jsystem.framework.system.SystemObjectImpl;
+
+import org.jsystemtest.mobile.core.AdbController;
+import org.jsystemtest.mobile.core.device.USBDevice;
+
+import com.cellrox.infra.enums.DeviceNumber;
+
+public class CellRoxDeviceManager extends SystemObjectImpl {
+	
+    private int privePort = 3435;
+    private int corpPort = 4321;
+    private String otaFileLocation;
+    private CellRoxDevice[] cellroxDevicesList;
+    private int numberOfDevices = 2;
+    
+    private AdbController adbController;
+    
+    public void init() throws Exception {
+    	super.init();
+    	cellroxDevicesList = new CellRoxDevice[numberOfDevices];
+
+    	adbController = AdbController.getInstance();
+    	USBDevice[] devices = adbController.waitForDevicesToConnect(numberOfDevices);
+    	
+    	//initing each one of the devices
+    	for(int index =0 ; index < devices.length ;index++){
+    		cellroxDevicesList[index] = new CellRoxDevice(privePort + index, corpPort + index, otaFileLocation, devices[index].getSerialNumber());
+    	}
+    	
+    	//to add to the summary properties
+    	
+    	getDevice(DeviceNumber.PRIMARY).addToTheSummarySystemProp();
+    	
+    	
+    }
+    
+
+    
+    public CellRoxDevice getDevice(DeviceNumber devNum) {
+    	return cellroxDevicesList[devNum.ordinal()];
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	/**
+	 * @return the otaFileLocation
+	 */
+	public String getOtaFileLocation() {
+		return otaFileLocation;
+	}
+	/**
+	 * @param otaFileLocation the otaFileLocation to set
+	 */
+	public void setOtaFileLocation(String otaFileLocation) {
+		this.otaFileLocation = otaFileLocation;
+	}
+	/**
+	 * @return the corpPort
+	 */
+	public int getCorpPort() {
+		return corpPort;
+	}
+	/**
+	 * @param corpPort the corpPort to set
+	 */
+	public void setCorpPort(int corpPort) {
+		this.corpPort = corpPort;
+	}
+	/**
+	 * @return the privePort
+	 */
+	public int getPrivePort() {
+		return privePort;
+	}
+	/**
+	 * @param privePort the privePort to set
+	 */
+	public void setPrivePort(int privePort) {
+		this.privePort = privePort;
+	}
+
+	/**
+	 * @return the numberOfDevices
+	 */
+	public int getNumberOfDevices() {
+		return numberOfDevices;
+	}
+
+	/**
+	 * @param numberOfDevices the numberOfDevices to set
+	 */
+	public void setNumberOfDevices(int numberOfDevices) {
+		this.numberOfDevices = numberOfDevices;
+	}
+    
+
+}
