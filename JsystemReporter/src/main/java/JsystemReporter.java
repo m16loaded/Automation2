9caf6c9eff1f5853046ccd07e8411792b7f2f0b9
orgar <orgarfunkel@gmail.com>
2014-02-03 14:53:41 +0200

    added the send mail inside

--- JsystemReporter/src/main/java/JsystemReporter.java
+++ JsystemReporter/src/main/java/JsystemReporter.java
@@ -7,7 +7,21 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -128,7 +142,7 @@ public class JsystemReporter {
 			final String from =  "cellrox99@gmail.com";
 			final String password = "cellrox2011";
 			
-			EmailSender.sendEmail(to, from, "Automation summary report", "Here the report of the automation", nameOfReport, password);
+			sendEmail(to, from, "Automation summary report", "Here the report of the automation", nameOfReport, password);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -136,6 +150,55 @@ public class JsystemReporter {
 	    
 		
 	}
+	
+	
+	public static void sendEmail(String to, final String username, String subject, String body, String fileName , final String password) {
+		  
+
+	    Properties props = new Properties();
+	    props.put("mail.smtp.auth", true);
+	    props.put("mail.smtp.starttls.enable", true);
+	    props.put("mail.smtp.host", "smtp.gmail.com");
+	    props.put("mail.smtp.port", "587");
+
+	    Session session = Session.getInstance(props,
+	            new javax.mail.Authenticator() {
+	                protected PasswordAuthentication getPasswordAuthentication() {
+	                    return new PasswordAuthentication(username, password);
+	                }
+	            });
+
+	    try {
+
+	        Message message = new MimeMessage(session);
+	        message.setFrom(new InternetAddress(username));
+	        message.setRecipients(Message.RecipientType.TO,
+	                InternetAddress.parse(to));
+	        message.setSubject(subject);
+	        message.setText(body);
+
+	        MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+	        Multipart multipart = new MimeMultipart();
+
+	        messageBodyPart = new MimeBodyPart();
+	        DataSource source = new FileDataSource(fileName);
+	        messageBodyPart.setDataHandler(new DataHandler(source));
+	        messageBodyPart.setFileName(fileName);
+	        multipart.addBodyPart(messageBodyPart);
+
+	        message.setContent(multipart);
+
+	        System.out.println("Sending");
+
+	        Transport.send(message);
+
+	        System.out.println("Done");
+
+	    } catch (MessagingException e) {
+	        e.printStackTrace();
+	    }
+}
  
 
 }
