1e88a34161c599322e8ca828ac58aa459c7a462e
orgar <orgarfunkel@gmail.com>
2014-02-03 11:58:03 +0200

    the Log.d and Log.i removed

--- uiautomatorServer-19/src/com/github/uiautomatorstub/Log.java
+++ uiautomatorServer-19/src/com/github/uiautomatorstub/Log.java
@@ -4,11 +4,11 @@ public class Log {
 	public static final String TAG = "UIAutomatorStub";
 	
 	public static void d(String msg) {
-		android.util.Log.d(TAG, msg);
+//		android.util.Log.d(TAG, msg);
 	}
 
 	public static void i(String msg) {
-		android.util.Log.i(TAG, msg);
+//		android.util.Log.i(TAG, msg);
 	}
 
 	public static void e(String msg) {
