11148902d556b8fe8e4031b9b2f21ed36987aa35
orgar <orgarfunkel@gmail.com>
2014-02-26 10:46:43 +0200

    firefox insted of chrom

--- cellrox-infra-project/src/main/java/com/cellrox/infra/WebDriverSO.java
+++ cellrox-infra-project/src/main/java/com/cellrox/infra/WebDriverSO.java
@@ -3,7 +3,7 @@ package com.cellrox.infra;
 import jsystem.framework.system.SystemObjectImpl;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 
@@ -15,9 +15,9 @@ public class WebDriverSO extends SystemObjectImpl {
 	public void init() throws Exception {
 		report.report("init");
 		super.init();
-//		System.setProperty("webdriver.firefox.driver","/");
-		System.setProperty("webdriver.chrome.driver", chromWebDriverLocation);
-		this.driver = new ChromeDriver(); //new FirefoxDriver();
+		System.setProperty("webdriver.firefox.driver","/");
+//		System.setProperty("webdriver.chrome.driver", chromWebDriverLocation);
+		this.driver = new FirefoxDriver();//new ChromeDriver(); //
 //		System.setProperty("webdriver.chrome.driver","/home/topq/dev/chromedriver");
 //		this.driver = new ChromeDriver();
 	}
