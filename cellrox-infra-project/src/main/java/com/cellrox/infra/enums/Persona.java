39b5260e78b09351126b03ea0345ab0d4ec52f59
orgar <orgarfunkel@gmail.com>
2014-03-18 11:54:29 +0200

    the nc added to the project

--- cellrox-infra-project/src/main/java/com/cellrox/infra/enums/Persona.java
+++ cellrox-infra-project/src/main/java/com/cellrox/infra/enums/Persona.java
@@ -3,7 +3,7 @@ package com.cellrox.infra.enums;
 public enum Persona {
 
 	PRIV("priv", "/data/containers/priv/data/local/tmp/"), CORP("corp",
-			"/data/containers/corp/data/local/tmp/");
+			"/data/containers/corp/data/local/tmp/"); 
 	String value;
 	String tmpLib;
 
