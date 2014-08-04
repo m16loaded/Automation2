05c5fd3b626e7df5d1dbd21993e069b3110e4846
orgar <orgarfunkel@gmail.com>
2014-08-04 18:28:41 +0300

    - LogParser.java added some functionalty

--- cellrox-infra-project/src/main/java/com/cellrox/infra/log/LogParser.java
+++ cellrox-infra-project/src/main/java/com/cellrox/infra/log/LogParser.java
@@ -2,6 +2,9 @@ package com.cellrox.infra.log;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.security.auth.login.LoginException;
 
 import jsystem.extensions.analyzers.text.CountText;
 import jsystem.framework.report.Reporter;
@@ -15,15 +18,32 @@ import com.cellrox.infra.object.LogParserExpression;
 public class LogParser extends SystemObjectOperation {
 
 	LogParserExpression[] expressions;
+	ArrayList<LogParserExpression> tmpExpressions;
+	File[] logs;
 
 	public LogParser(LogParserExpression[] expressions) {
 		super();
 		this.expressions = expressions;
 	}
+	
+	public LogParser() {
+		super();
+		tmpExpressions = new ArrayList<LogParserExpression>();
+	}
+	
+	public void addExpression(Color color, String expression, String niceName){
+		LogParserExpression logParserExpression = new LogParserExpression();
+		logParserExpression.setColor(color);
+		logParserExpression.setExpression(expression);
+		logParserExpression.setNiceName(niceName);
+		tmpExpressions.add(logParserExpression);
+	}
 
-	File[] logs;
 
 	public void validateLogs() throws IOException {
+		if (expressions==null){
+			expressions = tmpExpressions.toArray(new LogParserExpression[tmpExpressions.size()]);
+		}
 		for (File logFile : logs) {
 			String log = FileUtils.read(logFile);
 			for (LogParserExpression expression : expressions) {
@@ -32,7 +52,7 @@ public class LogParser extends SystemObjectOperation {
 				countText.analyze();
 				int result = countText.getActualCount();
 				if (result > 0) {
-					expression.setColor(Color.RED);
+					//expression.setColor(Color.RED);
 					log = log.replace(expression.getExpression(), "<b><font size=\"4\" "+expression.getHtmlColor()+">" + expression.getExpression()
 							+ "</font></b>");
 					report.report(
