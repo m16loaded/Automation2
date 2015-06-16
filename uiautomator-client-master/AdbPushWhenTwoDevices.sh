75415a856e4f94eb78db6723577d129b8bd0300b
itai.kaufman <ikaufman@cellrox.com>
2015-06-16 15:52:55 +0300

    Fixing bugs in adbPush script
    Fixing bugs in adbPush script

--- uiautomator-client-master/AdbPushWhenTwoDevices.sh
+++ uiautomator-client-master/AdbPushWhenTwoDevices.sh
@@ -9,5 +9,8 @@ IGNORESERIAL="08726194998cf3ad"
 for SERIAL in $(adb devices | grep -v $IGNORESERIAL | cut -f 1);
 do
 adb -s $SERIAL root
-adb -s $SERIAL push /home/topq/Downloads/nanomark2.apk /data/containers/priv/data/media/0/TEMP
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /data/containers/priv/data/media/0/TEMP
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.quicinc.vellamo2.apk /data/containers/priv/data/media/0/TEMP
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.android.chrome.apk /data/containers/priv/data/media/0/TEMP
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/org.zwanoo.android.speedtest.apk /data/containers/priv/data/media/0/TEMP
 done
