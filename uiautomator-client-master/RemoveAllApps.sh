989f37a113f49b84018460d41bc2a8529b2d5174
itai.kaufman <ikaufman@cellrox.com>
2015-09-20 18:47:01 +0300

    Fixing scripts/paths
    Fixing scripts/paths

---
+++ uiautomator-client-master/RemoveAllApps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Copy this script from git/automation to /home/topq/AutoScripts
+set -x
+
+
+IGNORESERIAL="08726194998cf3ad"
+
+
+for SERIAL in $(adb devices | grep -v $IGNORESERIAL | cut -f 1);
+do
+adb -s $SERIAL root
+adb -s $SERIAL shell rm -rf /personas/priv/data/app/nanomark2.apk /personas/priv/data/app/nanomark2-1 /personas/priv/data/app/com.android.chrome.apk /personas/priv/data/app/com.quicinc.vellamo2.apk /personas/priv/data/app/com.quicinc.vellamo2-1 /personas/priv/data/app/org.zwanoo.android.speedtest.apk /personas/priv/data/app/com.android.chrome-1 /personas/priv/data/app/org.zwanoo.android.speedtest-1
+
+done
