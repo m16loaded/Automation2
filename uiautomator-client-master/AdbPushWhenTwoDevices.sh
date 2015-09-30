58c5a35b1d4e28a7c72d92d6314d8cf3ed8c4293
itai.kaufman <ikaufman@cellrox.com>
2015-09-30 09:28:17 +0300

    changed path in adb push script
    changed path in adb push script

--- uiautomator-client-master/AdbPushWhenTwoDevices.sh
+++ uiautomator-client-master/AdbPushWhenTwoDevices.sh
@@ -9,8 +9,8 @@ IGNORESERIAL="08726194998cf3ad"
 for SERIAL in $(adb devices | grep -v $IGNORESERIAL | cut -f 1);
 do
 adb -s $SERIAL root
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /data/containers/priv/data/media/0/TEMP
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.quicinc.vellamo2.apk /data/containers/priv/data/media/0/TEMP
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.android.chrome.apk /data/containers/priv/data/media/0/TEMP
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/org.zwanoo.android.speedtest.apk /data/containers/priv/data/media/0/TEMP
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /personas/priv/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.quicinc.vellamo2.apk /personas/priv/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.android.chrome.apk /personas/priv/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/org.zwanoo.android.speedtest.apk /personas/priv/data/app
 done
