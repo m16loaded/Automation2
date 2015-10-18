f6f96db14d8e3a9246f23983b84da74775f0d80b
itai.kaufman <ikaufman@cellrox.com>
2015-10-18 15:38:46 +0300

    bugs in push script
    bugs in push script

--- uiautomator-client-master/AdbPushWhenTwoDevices.sh
+++ uiautomator-client-master/AdbPushWhenTwoDevices.sh
@@ -9,6 +9,7 @@ IGNORESERIAL="08726194998cf3ad"
 for SERIAL in $(adb devices | grep -v $IGNORESERIAL | cut -f 1);
 do
 adb -s $SERIAL root
+adb -s $SERIAL remount
 adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /personas/priv/data/app
 adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /personas/corp/data/app
 adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.quicinc.vellamo2.apk /personas/priv/data/app
