bb835892feb49dfe4dcc6f645025e2bfd452ff37
itai.kaufman <ikaufman@cellrox.com>
2016-01-11 15:06:41 +0200

    Fixing pushing apps script to support p1p2
    Fixing pushing apps script to support p1p2

--- uiautomator-client-master/AdbPushWhenTwoDevices.sh
+++ uiautomator-client-master/AdbPushWhenTwoDevices.sh
@@ -10,11 +10,11 @@ for SERIAL in $(adb devices | grep -v $IGNORESERIAL | cut -f 1);
 do
 adb -s $SERIAL root
 adb -s $SERIAL remount
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /personas/priv/data/app
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /personas/corp/data/app
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.quicinc.vellamo2.apk /personas/priv/data/app
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.android.chrome.apk /personas/priv/data/app
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/org.zwanoo.android.speedtest.apk /personas/priv/data/app
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/cxRilPolicy.apk /personas/priv/data/app
-adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/imoblife.androidsensorbox.apk /personas/priv/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /personas/p1/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/nanomark2.apk /personas/p2/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.quicinc.vellamo2.apk /personas/p1/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/com.android.chrome.apk /personas/p1/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/org.zwanoo.android.speedtest.apk /personas/p1/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/cxRilPolicy.apk /personas/p1/data/app
+adb -s $SERIAL push /home/topq/git/automation/uiautomator-client-master/imoblife.androidsensorbox.apk /personas/p1/data/app
 done
